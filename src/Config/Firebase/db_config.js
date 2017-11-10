625c0d349a68991f8f5cb88b9013a997cfa059ad
Brennan <bmart2913@gmail.com>
2017-11-10 00:59:10 -0500

    init commit

---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+        apiKey: "AIzaSyDPGP23DvBpcPjb6by3y6lJ_Dk1oOmA9Qo",
+        authDomain: "react-flashcards-841de.firebaseapp.com",
+        databaseURL: "https://react-flashcards-841de.firebaseio.com",
+        projectId: "react-flashcards-841de",
+        storageBucket: "",
+        messagingSenderId: "124414121999"
+};
